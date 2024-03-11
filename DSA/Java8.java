a6fb52cdb4025e8673f070b5e041cc4245354c69
SnehaPandey1993 <snehapandey0830@gmail.com>
2024-03-12 00:38:04 +0530

    Update for 12/3/2024

--- DSA/Java8.java
+++ DSA/Java8.java
@@ -38,6 +38,9 @@ public class Java8 {
         List<String> list=Arrays.asList("Java", "Python", "C#", "Java", "Kotlin", "Python");
         Map<String,Long> ls=list.stream().collect(Collectors.groupingBy(Function.identity(),Collectors.counting()));
         System.out.println(ls);
+        Map.Entry<String,Long> col=list.stream().collect(Collectors.groupingBy(Function.identity(),Collectors.counting())).entrySet().stream().filter(i->i.getValue()>1).max(Map.Entry.comparingByKey()).get();
+        System.out.println("MAX "+col.getValue()+" key "+col.getKey());
+        //15) Given a list of strings, sort them according to increasing order of their length?
         List<String> st=list.stream().distinct().toList().stream().sorted().toList();
         String sjoin=st.stream().collect(Collectors.joining(",","[","]"));
         st.stream().forEach(i-> System.out.print(i+" "));
@@ -74,7 +77,10 @@ public class Java8 {
     public void joinArray(){
         int[] a=new int[]{1,4,3,5};
         int[] b=new int[]{7,6,3,9};
-       int[] c=IntStream.concat(Arrays.stream(a),Arrays.stream(b)).distinct().sorted().toArray();
+        int[] c=IntStream.concat(Arrays.stream(a),Arrays.stream(b)).distinct().sorted().toArray();
+        Integer secmax= Arrays.stream(c).sorted().skip(Math.max(0,c.length-2)).findFirst().orElseThrow();
+        Integer secmin= Arrays.stream(c).sorted().skip(1).findFirst().orElseThrow();
+        System.out.println(secmax+" "+secmin);
         System.out.println(Arrays.toString(c));
     }
     @Test
@@ -89,4 +95,38 @@ public class Java8 {
             System.out.println("String is not anagram");
         }
     }
+    @Test
+    public void sumOfDigit(){
+        //13) Find sum of all digits of a number in Java 8?
+        int i= 156789;
+        //Integer sum=Stream.of(String.valueOf(i).split("")).collect(Collectors.summingInt(Integer::parseInt));
+       Integer sum=Arrays.asList(String.valueOf(i).split("")).stream().collect(Collectors.summingInt(Integer::parseInt));
+        System.out.println(sum);
+    }
+    @Test
+    public void sumAvg(){
+        int[] a = new int[] {45, 12, 56, 15, 24, 75, 31, 89};
+        int sum=Arrays.stream(a).sum();
+        System.out.println(sum);
+
+        double avg=Arrays.stream(a).average().getAsDouble();
+        System.out.println(avg);
+        List<Integer> list1 = Arrays.asList(71, 21, 34, 89, 56, 28);
+        List<Integer> list2 = Arrays.asList(12, 56, 17, 21, 94, 34);
+        list1.stream().filter(list2::contains).forEach(System.out::println);
+        int sumNatural=IntStream.rangeClosed(1,11).sum();
+        System.out.println("natural "+sumNatural);
+        int[] rev=IntStream.rangeClosed(1,a.length).map(i->a[a.length-i]).toArray();
+        System.out.println(Arrays.toString(rev));
+        IntStream.rangeClosed(1,10).map(i->i*2).forEach(System.out::print);
+    }
+
+    @Test
+    public void average(){
+        //19) How do you find sum of first 10 natural numbers?
+        String str = "Java Concept Of The Day";
+        String revStr=Arrays.stream(str.split(" ")).map(word->new StringBuffer(word).reverse()).collect(Collectors.joining(" "));
+        System.out.println(revStr);
+    }
+
 }
